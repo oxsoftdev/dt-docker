0859c7c8c267811308831b6352bd4c8bfa1c98bf
oxsoftdev <oxsoftdev@gmail.com>
2017-07-19 19:02:51 -0400

    added(entities): Public REST API

--- dtdocker/_assets/sql-scripts/mssql/bitso-ddl.sql
+++ dtdocker/_assets/sql-scripts/mssql/bitso-ddl.sql
@@ -10,6 +10,87 @@ use Bitso
 GO
 
 
+if (schema_id(N'api_public') is null) exec(N'create schema [api_public]');
+GO
+
+
+; create table [api_public].[Book]
+(
+    _id bigint identity(1,1)
+    , _timestamp decimal(20,8) not null
+    , _datetime datetime not null
+    --
+    , book nvarchar(32)
+    , minimum_amount decimal(18,8)
+    , maximum_amount decimal(18,8)
+    , minimum_price decimal(18,8)
+    , maximum_price decimal(18,8)
+    , minimum_value decimal(18,8)
+    , maximum_value decimal(18,8)
+    --
+    , constraint PK_api_Book primary key (_id)
+)
+;
+
+
+; create table [api_public].[Ticker]
+(
+    _id bigint identity(1,1)
+    , _timestamp decimal(20,8) not null
+    , _datetime datetime not null
+    --
+    , book nvarchar(32)
+    , volume decimal(18,8)
+    , high decimal(18,8)
+    , last decimal(18,8)
+    , low decimal(18,8)
+    , vwap decimal(18,8)
+    , ask decimal(18,8)
+    , bid decimal(18,8)
+    , created_at datetime
+    --
+    , constraint PK_api_Ticker primary key (_id)
+)
+;
+
+
+; create table [api_public].[Order]
+(
+    _id bigint identity(1,1)
+    , _timestamp decimal(20,8) not null
+    , _datetime datetime not null
+    --
+    , book nvarchar(32)
+    , price decimal(18,8)
+    , amount decimal(18,8)
+    , oid nvarchar(256)
+    , updated_at datetime
+    , sequence bigint
+    --
+    , constraint PK_api_Order primary key (_id)
+)
+;
+
+
+; create table [api_public].[Trade]
+(
+    _id bigint identity(1,1)
+    , _timestamp decimal(20,8) not null
+    , _datetime datetime not null
+    --
+    , book nvarchar(32)
+    , created_at datetime
+    , amount decimal(18,8)
+    , maker_side nvarchar(16)
+    , price decimal(18,8)
+    , tid bigint
+    --
+    , constraint PK_api_Trade primary key (_id)
+)
+;
+GO
+
+
 if (schema_id(N'ws') is null) exec(N'create schema [ws]');
 GO
 
@@ -59,8 +140,8 @@ GO
     --
     , book nvarchar(32)
     , tid bigint
-    , rate decimal(18,8)
     , amount decimal(18,8)
+    , rate decimal(18,8)
     , value decimal(18,8)
     , type nvarchar(4)
     , maker_order_id nvarchar(256)
